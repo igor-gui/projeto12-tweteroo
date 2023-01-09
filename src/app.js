c30735d5054f3c8ebc00667d1cb327ffb2b7b9b7
Igor Guimarães <igorgui.tec@gmail.com>
2023-01-09 15:57:28 -0300

    Cadastro - API

---
+++ src/app.js
@@ -0,0 +1,63 @@
+import express from "express";
+import cors from 'cors'
+import postUser from "./Controllers/postUser.controller.js";
+
+const server = express()
+server.use(express.json())
+server.use(cors())
+
+const users = []
+const tweets = []
+
+server.post('/sign-up', (req, res) => {
+    const { username, avatar } = req.body
+    if (users.find((e) => e.username === username)) {
+        res.status(409).send({ message: "Usuário não disponível" })
+    }
+    const user = {
+        username: username,
+        avatar: avatar
+    }
+    users.push(user)
+    res.send(users)
+})
+
+server.get('/sign-up', (req, res) => {
+    res.send(users)
+})
+
+server.post('/tweets', (req, res) => {
+    const result = "OK"
+
+    const { username, tweet } = req.body
+    if (!username || !tweet) {
+        result = "Nãoooo"
+        res.status(400).send({ message: result })
+
+    } else {
+        const user = users.find((e) => e.username === username)
+
+        const tuite = {
+            username: username,
+            avatar: user.avatar,
+            tweet: tweet
+        }
+        tweets.push(tuite)
+        res.status(200).send({ message: result })
+
+        const twits = []
+        for (let i = tweets.length - 10; i < tweets.length; i++) {
+            twits.push(tweets[i])
+        }
+
+        if (tweets.length >= 10) {
+            res.send(twits)
+        } else {
+            res.send(tweets)
+        }
+    }
+})
+
+const PORT = 5000
+
+server.listen(PORT, console.log(`O server está funcionando na porta ${PORT}`))
