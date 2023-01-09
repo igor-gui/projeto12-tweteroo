76aa61ff6ea2a8c2e111adab521be937f6770bbe
Igor Guimarães <igorgui.tec@gmail.com>
2023-01-09 16:04:40 -0300

    Postagem de tweets

--- src/app.js
+++ src/app.js
@@ -26,6 +26,41 @@ server.get('/sign-up', (req, res) => {
     res.send(users)
 })
 
+server.post('/tweets', (req, res) => {
+    const result = "OK"
+
+    const { username, tweet } = req.body
+    if (!username || !tweet) {
+        result = "UNAUTHORIZED"
+        res.status(400).send({ message: result })
+
+    } else {
+        const user = users.find((e) => e.username === username)
+        const twits = []
+
+        const tuite = {
+            username: username,
+            avatar: user.avatar,
+            tweet: tweet
+        }
+
+        tweets.push(tuite)
+        res.status(200).send({ message: result })
+
+        for (let i = tweets.length - 10; i < tweets.length; i++) {
+
+            twits.push(tweets[i])
+        }
+        //  Últimos 10 tweets
+
+        if (tweets.length >= 10) {
+            res.send(twits)
+        } else {
+            res.send(tweets)
+        }
+    }
+})
+
 
 
 const PORT = 5000
